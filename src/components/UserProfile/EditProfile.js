7034e37b4c392b5e768e0bf61e37d94a849b50d8
Dang Dat <khacdatdang@gmail.com>
2022-01-16 21:19:52 +0700

    update updateinfo

--- src/components/UserProfile/EditProfile.js
+++ src/components/UserProfile/EditProfile.js
@@ -15,6 +15,20 @@ function EditProfile() {
         gender :'',
         birthday : ''
     })
+
+    const formatDate = date => {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+    
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+    
+        return [year, month, day].join('-');
+    }
     useEffect(() => {
         axios.get(`/api/user_info`).then(res => {
             if (res.data.status=== 200 ){
@@ -31,30 +45,19 @@ function EditProfile() {
     const handleInput = e => {
         e.persist()
         setUserInfo({...userInfo, [e.target.name]: e.target.value})
+        console.log(userInfo);
     }
-    const [errorList, setErrorList] = useState({
-        name : '',
-        phone : '',
-        address : ''
-    })
     const updateProfile = e => {
         e.preventDefault()
-        const data = userInfo
-        const email = localStorage.getItem("auth_name")
+        const user_id = localStorage.getItem("user_id")
         
-        axios.post(`/api/update-user/${email}`, data).then(res => {
+        axios.put(`/api/user/${user_id}`, userInfo).then(res => {
             if (res.data.status === 200){
                 swal("Success",res.data.message, "success")
                 history.push('/userprofile')
             }
-            else if (res.data.status === 422)
-            {
-                setErrorList(res.data.errorList)
-                swal("Failed",res.data.message, "error")
-            }
-            else if (res.data.status === 404){
-                swal("Failed",res.data.message, "error")
-            }
+        }).catch(err => {
+            swal("Error", "Edit fail", "error")
         })
     }
 
@@ -64,17 +67,44 @@ function EditProfile() {
             <br/>
             <form className="row g-3" onSubmit = {updateProfile}>
             <div className="col-12">
-                <label className="form-label">Name</label>
-                <input className="form-control" name = "name"value = {userInfo.name} onChange = {handleInput} />
-            </div>
-            <div className="col-12">
-                <label className="form-label">Phone Number</label>
-                <input className="form-control" name = "phone" value = {userInfo.phone} onChange = {handleInput}/>
-            </div>
-            <div className="col-12">
-                <label className="form-label">Address</label>
-                <input className="form-control" name = "address"  value = {userInfo.address} onChange = {handleInput} />
-            </div>
+                    <label className="form-label">Name</label>
+                    <input className="form-control" name = "name" value = {userInfo.name} onChange={handleInput}/>
+                </div>
+                <div className="col-12">
+                    <label className="form-label">Email</label>
+                    <input className="form-control" name = "email" value ={userInfo.email}  onChange={handleInput}/>
+                </div>
+                <div className="col-12">
+                    <label className="form-label">Phone Number</label>
+                    <input className="form-control" name = "phone" value = {userInfo.phone} onChange={handleInput} />
+                </div>
+                <div className="col-12">
+                    <label className="form-label">Gender </label>
+                    {/* <input className="form-control" value = {userInfo.gender} onChange={handleInput} /> */}
+                    <select
+                                className="billing_textInput"
+                                name="gender"
+                                onChange={handleInput}
+                                defaultValue={userInfo.gender}>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="Orther">Other</option>
+                    </select>
+                </div>
+                <div className="col-12">
+                    <label className="form-label">Birthday </label>
+                    <input
+                                className="billing_textInput"
+                                type="date" name="birthday"
+                                placeholder="No information"
+                                defaultValue={formatDate(userInfo.birthday)}
+                                onChange={handleInput}
+                            />
+                </div>
+                <div className="col-12">
+                    <label className="form-label">Address</label>
+                    <input className="form-control" name = "address" value = {userInfo.address} onChange={handleInput}  />
+                </div>
             <button type ="submit" className = "btn btn-primary px-4 " > Submit</button>
             </form>
             <br/>
--- src/components/UserProfile/Profile.js
+++ src/components/UserProfile/Profile.js
@@ -6,19 +6,7 @@ import swal from 'sweetalert'
 
 function Profile() {
     const [loading, setloading] = useState(true)
-    const formatDate = date => {
-        var d = new Date(date),
-            month = '' + (d.getMonth() + 1),
-            day = '' + d.getDate(),
-            year = d.getFullYear();
     
-        if (month.length < 2)
-            month = '0' + month;
-        if (day.length < 2)
-            day = '0' + day;
-    
-        return [year, month, day].join('-');
-    }
     const [userInfo, setUserInfo] = useState({
         name : '',
         email : '',
